de368390e7694883da11c2156ac84c319dac58d3
Ivan <ivanndahiro02@gmail.com>
2023-06-24 19:55:25 +0100

    Commented code

--- Timer.js
+++ Timer.js
@@ -1,43 +1,42 @@
-function nowTime(){
+function nowTime(){ //Grabs the current time when button pressed and updates every second
     document.getElementById("Time").innerHTML=Date();
     setInterval(nowTime,1000);
 }
-function count(){
-    x = document.getElementById("fulltime").value;
-    dateObj = new Date(x);
+function count(){ //Main function to calculate the countdown
 
-    m=dateObj.getMonth()+1;
-    d=dateObj.getDate();
-    y=dateObj.getFullYear();
-    h=dateObj.getHours();
-    mi=dateObj.getMinutes();
-    
-    selectedDate=m+" "+d+", "+y+" "+h+":"+mi;
+    //Takes input
+    input = document.getElementById("fulltime").value;
 
-    const futureTime = new Date(selectedDate).getTime();
-    const currentTime = new Date().getTime();
-    const diff = futureTime - currentTime;
+    //Grabs input and current time, converts to Date format and calculates the difference
+    futureTime = new Date(input).getTime();
+    currentTime = new Date().getTime();
+    diff = futureTime - currentTime;
 
-    const sec=1000;
-    const min=sec*60;
-    const hour=min*60;
-    const day=hour*24;
-    const month=day*30.4117;
-    const year=month*12;
+    //Formats each unit in milliseconds
+    sec=1000;
+    min=sec*60;
+    hour=min*60;
+    day=hour*24;
+    month=day*30.4117;
+    year=month*12;
 
-    const Years = Math.floor(diff/year);
-    const Months = Math.floor((diff%year)/month);
-    const Days = Math.floor((diff%month)/day);
-    const Hours = Math.floor((diff%day)/hour);
-    const Minutes = Math.floor((diff%hour)/min);
-    const Seconds = Math.floor((diff%min)/sec);
+    //Calculates each unit for the difference and outputs in a rounded down value
+    Years = Math.floor(diff/year);
+    Months = Math.floor((diff%year)/month);
+    Days = Math.floor((diff%month)/day);
+    Hours = Math.floor((diff%day)/hour);
+    Minutes = Math.floor((diff%hour)/min);
+    Seconds = Math.floor((diff%min)/sec);
 
+    //Displays the countdown value in readable text
     document.getElementById("MyTime").innerHTML= Years + " years " + Months + " months " + Days + " days "
     + Hours + " hours " + Minutes + " minutes " + Seconds + " seconds ";
 
+    //Once the countdown is done, outputs a string
     if (diff<0){
     document.getElementById("MyTime").innerHTML="Done!";
     }
 
-    setInterval(count, 1000);
+    //Updates the countdown each second
+    setInterval(count,1000);
 }
